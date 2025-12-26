91c30ba4ee950f3ae53026974cfc180a61926605
dant132 <dant13@vse.cz>
2025-12-26 02:09:00 +0100

    Initial commit

---
+++ src/main/java/com/example/ghactionsmonitor/GhActionsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghactionsmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhActionsMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ghactionsmonitor/GhActionsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghactionsmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
