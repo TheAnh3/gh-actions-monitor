d3cc5e2d9c0e04ea4aac00e3848a5f453bc678cc
dant132 <dant13@vse.cz>
2025-12-28 01:57:58 +0100

    MonitoringService to list WorflowRuns

--- src/main/java/com/example/ghactionsmonitor/service/MonitorService.java
+++ src/main/java/com/example/ghactionsmonitor/service/MonitorService.java
@@ -1,9 +1,39 @@
 package com.example.ghactionsmonitor.service;
 
+import com.example.ghactionsmonitor.client.GitHubClient;
+import com.example.ghactionsmonitor.model.WorkflowRun;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class MonitorService {
-    public void startMonitoring(String repo, String token){
-        System.out.println("Starting monitoring for repo: " + repo);
+    private final GitHubClient gitHubClient;
+
+    MonitorService(GitHubClient gitHubClient){
+        this.gitHubClient = gitHubClient;
+    }
+
+
+    public void startMonitoring(String repo, String token) {
+        String[] parts = repo.split("/");
+        if (parts.length != 2) {
+            System.err.println("Repo musí být ve formátu owner/repo");
+            return;
+        }
+        String owner = parts[0];
+        String repoName = parts[1];
+
+        List<WorkflowRun> runs = gitHubClient.listWorkflowRuns(owner, repoName, token);
+        if (runs.isEmpty()) {
+            System.out.println("No Workflow Runs found");
+        } else {
+            System.out.println("WorkflowRuns Found:");
+            runs.forEach(run -> System.out.printf(
+                    "ID: %d | Name: %s | Branch: %s | Commit: %s | Status: %s | Actor: %s | Started: %s | Completed: %s%n",
+                    run.id(), run.name(), run.branch(), run.commitSHA(), run.status(), run.actorLogin(),
+                    run.startedAt(), run.completedAt()
+            ));
+        }
     }
 }
